60ab5112f61346fa93ed94660d28d2e578fa9c26
mg-the-programmer <alphagameroffical@gmail.com>
2020-12-07 19:58:55 +0530

    Builder vs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./styles.css";
-import details from "./details.json";
 import "./App.css";
+import Builder from "Builder.js";
 
 export default function App() {
   return (
     <div className="App">
+      <Builder />
       <textarea name="message" rows="10" cols="30" style={{ marginTop: 10 }}>
         The cat was playing in the garden.
       </textarea>
