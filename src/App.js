0913d4921dc6c80673ff5e6615155e56e6bc807a
mg-the-programmer <70936350+mg-the-programmer@users.noreply.github.com>
2020-12-07 19:50:20 +0530

    builder

--- src/App.js
+++ src/App.js
@@ -6,52 +6,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      {/* Header */}
-      <h1 className="header">{details.Title}</h1>
-
-      {/* Input Boxs and their Labels */}
-      {details.Attributes.map((postDetail) => {
-        let { Name, Type, Size, DropdownValues } = postDetail;
-
-        if (Type === "SecretTextBox") {
-          return (postDetail.Type = "password");
-        }
-
-        return (
-          <div className="inputContainer">
-            {/* Label */}
-            <h3>{Name + ":"}</h3>
-
-            {/* InputBox */}
-            {(Type === "text" || Type === "password") && (
-              <input type={Type} className="inputBox" size={Size} />
-            )}
-
-            {/* Dropdown Inputbox */}
-            {Type === "DropDown" && (
-              <div>
-                <input list="dropDown" type="text" />
-                {postDetail.DropdownValues.map((value) => {
-                  let { DisplayValue } = value;
-                  return (
-                    <div>
-                      <datalist id="dropDown">
-                        <option value={DisplayValue}></option>
-                        <option value={DisplayValue}></option>
-                        {/* <p>{DisplayValue}</p> */}
-                      </datalist>
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-
-            {/* Radio Button */}
-          </div>
-        );
-      })}
-      {/* Buttons */}
-      <button type="submit">{details.ActionDisplayName}</button>
+      <textarea name="message" rows="10" cols="30" style={{ marginTop: 10 }}>
+        The cat was playing in the garden.
+      </textarea>
     </div>
   );
 }
